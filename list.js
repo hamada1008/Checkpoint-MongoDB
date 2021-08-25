f69b6a31e862baff55dbf6c37eb8e88c762af96a
Oussama <oussamahamada@yahoo.com>
2021-08-25 16:21:10 +0100

    first commit

---
+++ list.js
@@ -0,0 +1,35 @@
+db.contactlist.insertMany([
+  {
+    id: 1,
+    lName: "Ben Lahmer",
+    fName: "Fares",
+    email: "fares@gmail.com",
+    age: 26,
+  },
+  {
+    id: 2,
+    lName: "Kefi",
+    fName: "Seif",
+    email: "kefi@gmail.com",
+    age: 15,
+  },
+  {
+    id: 3,
+    lName: "Fatnassi",
+    fName: "Sarra",
+    email: "sarra.f@gmail.com",
+    age: 40,
+  },
+  {
+    id: 4,
+    lName: "Ben Yahiar",
+    fName: "Rym",
+    age: 4,
+  },
+  {
+    id: 5,
+    lName: "Cherif",
+    fName: "Sami",
+    age: 3,
+  },
+]);
